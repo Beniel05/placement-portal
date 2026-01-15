ea3901d29c3e12c376874d21a7ab8a1b0510b58e
beniel05 <benielebenezer@gmail.com>
2026-01-15 10:46:03 +0530

    Day 3: PostgreSQL and Prisma setup with initial User and StudentProfile schema

---
+++ backend/prisma/migrations/20260115051025_init_user_student/migration.sql
@@ -0,0 +1,36 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('STUDENT', 'COORDINATOR', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "clerkUserId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "StudentProfile" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "department" TEXT NOT NULL,
+    "cgpa" DOUBLE PRECISION NOT NULL,
+    "skills" TEXT NOT NULL,
+
+    CONSTRAINT "StudentProfile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_clerkUserId_key" ON "User"("clerkUserId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "StudentProfile_userId_key" ON "StudentProfile"("userId");
+
+-- AddForeignKey
+ALTER TABLE "StudentProfile" ADD CONSTRAINT "StudentProfile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
